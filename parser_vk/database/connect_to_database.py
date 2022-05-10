eec0cafd47b5dc2d503ca66ad56981e06085d642
Qvert <glscharow@yandex.ru>
2022-05-10 22:25:00 +0300

    Fix

--- parser_vk/database/connect_to_database.py
+++ parser_vk/database/connect_to_database.py
@@ -1,6 +1,6 @@
 import psycopg2
-from parser_vk import config
+import os
 
 
 # Подключение к базе данных
-connection = psycopg2.connect(config.DATABASE_URL, sslmode="require")
+connection = psycopg2.connect(os.environ['DATABASE_URL'], sslmode="require")
